c4a0475b6e7febb6d6669b06e835e51f694eb803
Michael Contino <Cont512n02@live.com>
2021-02-21 00:34:54 -0600

    added json output option and issue source

--- harvester.py
+++ harvester.py
@@ -6,6 +6,7 @@ import requests
 import docker
 import logging
 import argparse
+import json
 
 class harvester():
     def __init__(self):
@@ -53,14 +54,14 @@ class harvester():
                         if key in env.name.lower():
                             if env.value_from is None:
                                 logger.issue('Found potential credential {0} in env of container {1} running in pod {2} in namespace {3}'.format(env.name, container, pod, pod_map[pod]['namespace']))
-                                reported_issues.append({'name': env.name, 'value': env.value, 'container': container, 'pod': pod, 'namespace': pod_map[pod]['namespace'], 'key': key})
+                                reported_issues.append({'name': env.name, 'value': env.value, 'container': container, 'pod': pod, 'namespace': pod_map[pod]['namespace'], 'key': key, 'source': 'podEnv'})
                             else:
                                 if env.value_from.config_map_key_ref is None:
                                     continue
                                 else:
                                     tmp_configmap = self.core_client.read_namespaced_config_map(env.value_from.config_map_key_ref.name, pod_map[pod]['namespace'])
                                     logger.issue('Found potential credential {0} in env configmap of container {1} running in pod {2} in namespace {3}'.format(env.value_from.config_map_key_ref.key, container, pod, pod_map[pod]['namespace']))
-                                    reported_issues.append({'name': env.value_from.config_map_key_ref.key, 'value': tmp_configmap.data[env.value_from.config_map_key_ref.key], 'container': container, 'pod': pod, 'namespace': pod_map[pod]['namespace'], 'key': key})
+                                    reported_issues.append({'name': env.value_from.config_map_key_ref.key, 'value': tmp_configmap.data[env.value_from.config_map_key_ref.key], 'container': container, 'pod': pod, 'namespace': pod_map[pod]['namespace'], 'key': key, 'source': 'configmapEnv'})
 
                 dockerclient = docker.APIClient()
                 if pod_map[pod]['containers'][container]['version'] is None:
@@ -81,7 +82,7 @@ class harvester():
                     for key in credkey:
                         for tmp_env in tmp_mainifest['ContainerConfig']['Env']:
                             if key in tmp_env.split('=')[0].lower():
-                                reported_issues.append({'name': tmp_env.split('=')[0], 'value': tmp_env.split('=')[1], 'container': container, 'pod': pod, 'namespace': pod_map[pod]['namespace'], 'key': key})
+                                reported_issues.append({'name': tmp_env.split('=')[0], 'value': tmp_env.split('=')[1], 'container': container, 'pod': pod, 'namespace': pod_map[pod]['namespace'], 'key': key, 'source': 'manifestEnv'})
                                 logger.issue('Found a potential credential {0} in the manifest env of container {1} running in pod {2} in namespace {3}'.format(tmp_env.split('=')[0], container, pod, pod_map[pod]['namespace']))
         return reported_issues
 
@@ -117,6 +118,8 @@ if __name__ == '__main__':
     # Parse out all of the command line arguments
     parser = argparse.ArgumentParser(description='Try to gather potential creds from kubernetes')
     parser.add_argument('-v', '--verbose', help='Whether or not to display additional log information', required=False, action='store_true')
+    parser.add_argument('-w', '--write', help='Wether to write a log file, can be used with -0 to specify name/location', required=False, action='store_true')
+    parser.add_argument('-o', '--outfile', help='The file to write results (needs to be writable for current user)', required=False, default='harvester.json')
     args = parser.parse_args()
     if args.verbose:
         logging.basicConfig(level=logging.INFO, format='[%(levelname)s]: %(message)s')
@@ -132,3 +135,7 @@ if __name__ == '__main__':
     reported_issues = new_harvester.parse_pod_map_for_creds(pod_map)
     cloud_map = new_harvester.try_to_gather_cloud_tokens()
     logger.info('Harvester completed with {0} potential credentials identified'.format(len(reported_issues) + len(cloud_map)))
+    if args.write:
+        out_file = open(args.outfile, "w")
+        json.dump({'issues': reported_issues}, out_file, indent=4)
+        out_file.close()
